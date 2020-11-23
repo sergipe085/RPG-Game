676f9171958adf4c427463b182d0272eb2d701e8
Sérgio <71034184+Cangaceiro15@users.noreply.github.com>
2020-11-23 20:55:15 -0300

    Initial Player Movement
    Setup To Character Movement

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.position, Time.deltaTime * 5f);
+    }
+}
---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [Header("COMPONENTS")]
+    NavMeshAgent navMeshAgent;
+    Animator anim;
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            MoveToCursor();
+        }
+
+        UpdateAnimator();
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            navMeshAgent.SetDestination(hit.point);
+        }
+    }
+
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = navMeshAgent.velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        anim.SetFloat("ForwardSpeed", localVelocity.z);
+    }
+}
